f42be6d0bb528c3d000673eb71f0acef4d1ca731
Aaron Diez <aronpes94@gmail.com>
2018-05-16 15:12:10 +0200

    refactor with flake8 isort

--- core/factories.py
+++ core/factories.py
@@ -1,7 +1,9 @@
 import factory
-from core import models
-from .models import Animal, Person, Foster, AdopterFamily
 import factory.fuzzy
+
+from core import models
+
+from .models import AdopterFamily, Animal, Foster, Person
 
 animal_state = [state[0] for state in models.Animal.STATE_CHOICES]
 animal_sex = [sex[0] for sex in models.Animal.SEX_CHOICES]
@@ -9,7 +11,6 @@ animal_type = [type[0] for type in models.Animal.ANIMAL_TYPE_CHOICES]
 
 
 class AnimalFactory(factory.django.DjangoModelFactory):
-    
     class Meta:
         model = Animal
 
@@ -26,15 +27,13 @@ class PersonFactory(factory.django.DjangoModelFactory):
 
     class Meta:
         model = Person
-    
     name = factory.Faker('first_name')
     last_name = factory.Faker('last_name')
     phone = factory.Faker('phone_number')
     address = factory.Faker('address')
 
-    
-class FosterFactory(factory.django.DjangoModelFactory):
 
+class FosterFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Foster
 
