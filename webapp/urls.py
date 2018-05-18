1624480b366f1026ef0b5976d090fc07b790f14a
Aaron Diez <aronpes94@gmail.com>
2018-05-18 11:58:31 +0200

    add pagination

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,12 +1,14 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
 
 urlpatterns = [
-    path('', views.HomeView.as_view(), name='Home'),
+    path('', views.HomeView, name='Home'),
     path('frequent-questions', views.FrequentQuestionsView.as_view(),
          name='FrequentQuestions'),
     path('about-us', views.AboutUsView.as_view(), name='AboutUs'),
     path('help', views.HelpView.as_view(), name='Help'),
-    path('contact', views.ContactView.as_view(), name='Contact')
+    path('contact', views.ContactView.as_view(), name='Contact'),
+    re_path(r'^animal/(?P<pk>\d+)/detail/', views.AnimalDetail.as_view(),
+            name='animal_detail'),
 ]
