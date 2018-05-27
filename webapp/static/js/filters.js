6d9639a9cf38ce990feff13a2da347ea46c2f298
Aaron Diez <aronpes94@gmail.com>
2018-05-27 19:24:32 +0200

    add filter auto submit

---
+++ webapp/static/js/filters.js
@@ -0,0 +1,14 @@
+
+    $(".sex").on('click', function () {
+        $( "#form" ).submit();
+    });
+    $(".animal_type").on('click', function () {
+        $( "#form" ).submit();
+        
+    });
+
+    $(".size").on('click', function () {
+        $( "#form" ).submit();
+        
+    });
+
