0923f7222edbdecb3423a43d050a0f36851cb663
Aaron Diez <aronpes94@gmail.com>
2018-06-03 00:47:36 +0200

    fix production settings

--- animal_shelter/settings/production.py
+++ animal_shelter/settings/production.py
@@ -33,9 +33,9 @@ EMAIL_USE_SSL = email_config['EMAIL_USE_SSL']
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-AWS_ACCESS_KEY_ID = os.env['AWS_ACCESS_KEY_ID']
-AWS_SECRET_ACCESS_KEY = os.env['AWS_SECRET_ACCESS_KEY']
-AWS_STORAGE_BUCKET_NAME = os.env['AWS_STORAGE_BUCKET_NAME']
+AWS_ACCESS_KEY_ID = os.getenv['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.getenv['AWS_SECRET_ACCESS_KEY']
+AWS_STORAGE_BUCKET_NAME = os.getenv['AWS_STORAGE_BUCKET_NAME']
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_HEADERS = {  # see http://developer.yahoo.com/performance/rules.html#expires
     'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
